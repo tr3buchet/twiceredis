92b537295a969d6752cb2d831b6460dd7eed2937
Trey Morris <treyemorris@gmail.com>
2016-03-18 14:23:07 -0500

    fixed another derp

--- twiceredis/client.py
+++ twiceredis/client.py
@@ -231,13 +231,14 @@ class TwiceRedis(object):
 
     """
     generic_error = exceptions.RedisError
+    _sko = {socket.TCP_KEEPIDLE: 1,
+            socket.TCP_KEEPINTVL: 3,
+            socket.TCP_KEEPCNT: 5}
     DEFAULT_POOL_KWARGS = {'check_connection': True,
                            'socket_timeout': 15,
                            'socket_connect_timeout': 5,
                            'socket_keepalive': True,
-                           'socket_keeplive_options': {socket.TCP_KEEPIDLE: 1,
-                                                       socket.TCP_KEEPINTVL: 3,
-                                                       socket.TCP_KEEPCNT: 5}}
+                           'socket_keepalive_options': _sko}
     DEFAULT_SENTINEL_KWARGS = {'min_other_sentinels': 0,
                                'socket_timeout': 5,
                                'socket_connect_timeout': 5}
